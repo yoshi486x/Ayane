7cd479a5e1c384967d94d923cfbbb315e4b6b294
Yoshiki Nakagawa <yoshiki.nakagawa10@gmail.com>
2022-02-05 23:44:08 +0900

    modify setting import

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,16 @@
+import functools
+import os
+import dotenv
 
 
 class Settings:
-    engine_path: str = "../try-ayane/YaneuraOu/engine/suisho4kai/Suisho4kai_avx2.exe"
+    config = dotenv.load_dotenv(".env")
+    engine_path: str = os.getenv("ENGINE_PATH")
+    if engine_path is None:
+        print("Please set ENGINE_PATH environment variable.")
+        exit(1)
+    kifu_path: str = os.getenv("KIFU_PATH")
 
+@functools.lru_cache()
 def get_settings():
     return Settings()
