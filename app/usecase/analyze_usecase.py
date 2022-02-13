512ad8ce0cc4289d0615b8a3c2fb0c42912e47f0
Yoshiki Nakagawa <yoshiki.nakagawa10@gmail.com>
2022-02-13 16:39:22 +0900

    wip

--- app/usecase/analyze_usecase.py
+++ app/usecase/analyze_usecase.py
@@ -9,35 +9,26 @@ settings = get_settings()
 
 
 class AnalyzeUsecase:
-    @staticmethod
-    def hoge(filename):
-        # import kif
-        print('path: ', settings.kifu_path + filename)
-        kif = shogi.KIF.Parser.parse_file(settings.kifu_path + filename)[0]
-        if kif is None:
-            print('kif is None')
-            exit(1)
-        print('kif:', kif)
-        print('black:', kif['names'][shogi.BLACK])
-        print('white:', kif['names'][shogi.WHITE])
-        kif_moves = kif['moves']
 
-        # set engine
-        usi = UsiEngine(debug_print=True)
-        # engine_options = EngineOptions(hash=4096, threads=4)
-        engine_options = EngineOptions(hash=256, threads=4)
-        usi.set_engine_options(engine_options.suisho())
-        usi.connect(settings.engine_path)
+    @staticmethod
+    def main(filename):
+        kif = AnalyzeUsecase.import_kif(filename)
+        kif_moves = kif['moves']
+        kif_sfen = kif['sfen']
+        usi = AnalyzeUsecase.define_engine(kif_sfen)
 
         # set position
-        usi_position = f"sfen {kif['sfen']}"
+        usi_position = f"sfen {kif_sfen}"
         usi.usi_position(usi_position)
-
         usi.send_command('multipv 1')
-        usi.usi_go_and_wait_bestmove("btime 0 wtime 0 byoyomi 5000")
+        usi.usi_go_and_wait_bestmove("btime 0 wtime 0 byoyomi 1000")
         pv = usi.think_result.pvs[0]
 
         print(pv.pv)
+
+        # define board
+        board = shogi.Board()
+        # board.push()
 
         usi_position += f" moves"
         # for move in kif_moves:
@@ -47,11 +38,83 @@ class AnalyzeUsecase:
             usi_position += f" {move}"
             usi.usi_position(usi_position)
             # usi.send_command('multipv 1')
-            usi.usi_go_and_wait_bestmove("btime 0 wtime 0 byoyomi 5000")
+            usi.usi_go_and_wait_bestmove("btime 0 wtime 0 byoyomi 1000")
             pv = usi.think_result.pvs[0]
-            print(pv.pv)
+            # AnalyzeUsecase.pv_handler(pv)
+            # print('pvs:', usi.think_result.pvs)
+            # print('pv:', pv)
+            # print('pv.pv:', pv.pv)
+            # print('type(pv.pv):', type(pv.pv))
             
-            if i == 5:
+            pv_moves = pv.pv.split(' ')
+            # print(pv_moves)
+            pv_len = len(pv_moves)
+            # 読み筋を取り込む
+            # for pv_move in pv_moves:
+            for j, pv_move in enumerate(pv_moves):
+                # parse pv
+                shogi_move = shogi.Move.from_usi(pv_move)
+                from_square, to_square, promotion, drop_piece_type = shogi_move.from_square, shogi_move.to_square, shogi_move.promotion, shogi_move.drop_piece_type
+
+                # from_square_name = shogi.SQUARE_NAMES[shogi_move.from_square]
+                # to_square_name = shogi.SQUARE_NAMES[shogi_move.to_square]
+                if from_square is None and to_square is not None:
+                    # 打つ
+                    print(f'{i}: drop_piece_type:', drop_piece_type)
+                    # print(f'{i}: jp_piece_type:', shogi.PIECE_JAPANESE_SYMBOLS[drop_piece_type])
+                else:
+                    from_piece = board.piece_at(from_square)
+                    if from_piece is None:
+                        print(f'{i}, {j}: no from_piece, {pv_move}')
+                        continue
+                    print(f'{i}, {j}: from_piece:', from_piece.piece_type)
+                    # print('from_piece:', shogi.PIECE_JAPANESE_SYMBOLS[from_piece.piece_type])
+                    if promotion:
+                        # 成る
+                        pass
+                    else:
+                        pass
+
+                board.push(shogi_move)
+            
+            # 読み筋で取り込んだ手をすべて待ったする
+            for _ in range(len(pv_moves)):
+                board.pop()
+            
+            
+            if i == 50:
                 break
-        
+
         usi.disconnect()
+
+
+    @staticmethod
+    def import_kif(filename):
+        # import kif
+        print('path: ', settings.kifu_path + filename)
+        kif = shogi.KIF.Parser.parse_file(settings.kifu_path + filename)[0]
+        if kif is None:
+            print('kif is None')
+            exit(1)
+        print('kif:', kif)
+        print('black:', kif['names'][shogi.BLACK])
+        print('white:', kif['names'][shogi.WHITE])
+
+        return kif
+
+    @staticmethod
+    def define_engine(sfen: str):
+        # set engine
+        usi = UsiEngine(debug_print=True)
+        # engine_options = EngineOptions(hash=4096, threads=4)
+        engine_options = EngineOptions(hash=256, threads=4)
+        usi.set_engine_options(engine_options.suisho())
+        usi.connect(settings.engine_path)
+
+        return usi
+
+    @staticmethod
+    def pv_handler(pv: str):
+        pv_moves = pv.split(' ')
+        print(pv_moves)
+        # Copy Board or use the board and pop
--- tests/usecase/test_analyze_usecase.py
+++ tests/usecase/test_analyze_usecase.py
@@ -7,5 +7,20 @@ from src.settings import get_settings
 settings = get_settings()
 
 class TestAnalyzeUsecase(unittest.TestCase):
-    def test_hoge(self):
-        AnalyzeUsecase.hoge('C4ke/2022-02-05/C4ke-oku002211-20220205_165555.kif')
+    def test_main(self):
+        try:
+            AnalyzeUsecase.main('C4ke/2022-02-05/C4ke-oku002211-20220205_165555.kif')
+        except:
+            exit(1)
+
+    def test_import_kif(self):
+        kif = AnalyzeUsecase.import_kif('C4ke/2022-02-05/C4ke-oku002211-20220205_165555.kif')
+        print('kif:', kif)
+
+    def test_define_engine(sef):
+        usi = AnalyzeUsecase.define_engine('lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1')
+        usi.disconnect()
+
+    def test_pv_handler(self):
+        pv = '7i7h 8c8d 3i4h 8d8e 8h7g 7a6b 4i5h 5c5d 4g4f 3a3b 4h4g 3b4c 4f4e 6b5c 4e4d 5c4d 2h4h 8e8f 8g8f 4a3b 5i6h P*8e 6h7i 8e8f P*8h 8b8e 4g5f'
+        AnalyzeUsecase.pv_handler(pv)
