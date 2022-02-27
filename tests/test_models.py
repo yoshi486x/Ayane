4f1aaecf56a32dc75abe63925b8ad402d90daff6
Yoshiki Nakagawa <yoshiki.nakagawa10@gmail.com>
2022-02-27 12:40:51 +0900

    wip

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,6 +6,12 @@ from src.settings import get_settings
 
 settings = get_settings()
 
-class TestKFK(unittest.TestCase):
+class TestUtils(unittest.TestCase):
     def test_generate(self):
         Utils.generate_kfk('C4ke/2022-02-05/C4ke-oku002211-20220205_165555.kif')
+
+
+class TestKFK(unittest.TestCase):
+    def test_generate_xml_str(self):
+        KFK()
+        
